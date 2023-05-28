be5cc9d1b047d010e21390e28c5f242824136cc0
Nikolai Zoller <nikolai.zoller@hotmail.com>
2023-05-28 19:42:52 +0200

    Add: Header template

---
+++ Template_Header.m
@@ -0,0 +1,11 @@
+% Skript zur ...
+% Ersteller:    Nikolai Zoller
+% Histroy:      TT.MM.JJJJ  zolln1  Ersterstellung
+% Bemerkung:    
+% ------------------------------------------------------------------------
+
+% Script for ...
+% Creator:      Nikolai Zoller
+% Histroy:      TT.MM.JJJJ  zolln1  Initial creation
+% Comment:      
+% ------------------------------------------------------------------------
